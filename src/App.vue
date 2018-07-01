58f3cafc1177f822c7e7bb43a014d666ee2d887c
ridwanrahman <ridwan_rahman@ymail.com>
2018-07-01 23:05:36 +0600

    First commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <show-blogs></show-blogs>
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog.vue';
+import showBlogs from './components/showBlogs.vue';
+
+
+export default {
+  name: 'app',
+  components: {
+    'add-blog': addBlog,
+    'show-blogs': showBlogs
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: 'Nunito SemiBold';
+}
+
+</style>
